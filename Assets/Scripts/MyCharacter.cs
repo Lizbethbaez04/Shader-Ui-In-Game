fc8ccf0e8022dc4aa92e64ac0122d847f0fc104f
Lizbethbaez04 <42244092+Lizbethbaez04@users.noreply.github.com>
2019-05-01 16:02:19 -0500

    Sliders

---
+++ Assets/Scripts/MyCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacter : MonoBehaviour
+{
+    Renderer render;
+    [SerializeField]
+    float ColorPower = 1f;
+
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+
+    }
+
+    private void Update()
+    {
+        render.material.SetFloat("_ColorPower", ColorPower);
+    }
+}
